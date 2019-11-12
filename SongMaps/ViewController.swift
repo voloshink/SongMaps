2c478a16534804ff263d8f40a777bc5a1644e22f
Kirill Voloshin <voloshin.k@husky.neu.edu>
2019-11-11 21:39:05 -0500

    init commit

---
+++ SongMaps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SongMaps
+//
+//  Created by Polecat on 11/11/19.
+//  Copyright © 2019 Polecat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
